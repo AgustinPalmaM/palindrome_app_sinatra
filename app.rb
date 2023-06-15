c0fbbcf4b03d8bc5482df4ad985375476daff3dd
AgustinPalmaM <palma.agustin7@gmail.com>
2023-06-15 15:36:45 -0400

    Add dinamic title assign depends of the title variable declared into the controller

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 
 get '/' do
+  @title = "Index"
   erb :index
 end
 
 get '/about' do
+  @title = "About"
   erb :about
 end
 
 get '/palindrome' do
+  @title = "Palindrome"
   erb :palindrome
 end
--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -14,19 +14,23 @@ class PalindromeAppTest < Minitest::Test
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
     assert_equal 'Sample Sinatra App', doc(last_response).at_css('h1').content
+    assert_equal "Palindrome App | Index", doc(last_response).at_css('title').content
   end
-
+  
   def test_about
     get '/about'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
     assert_equal 'About', doc(last_response).at_css('h1').content
+    assert_equal "Palindrome App | About", doc(last_response).at_css('title').content
   end
-
+  
   def test_palindrome
     get '/palindrome'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
     assert_equal 'Palindrome Detector', doc(last_response).at_css('h1').content
+    assert_equal "Palindrome App | Palindrome", doc(last_response).at_css('title').content
   end
+
 end
